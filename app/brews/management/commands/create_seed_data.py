7901da6c18a4e8f60c6bbfdf7abc3c80f6fcb2ff
Caleb Tubbs <72274511+Tbubbs662@users.noreply.github.com>
2026-07-14 22:45:28 -0400

    Added create_seed_data command.

---
+++ app/brews/management/commands/create_seed_data.py
@@ -0,0 +1,15 @@
+from django.core.management.base import BaseCommand # type: ignore
+from django.contrib.auth.models import User # type: ignore
+from brews.models import Recipe, BrewSession
+
+class Command(BaseCommand):
+    help = 'This command is to create seed data for the database. Creates a demo user that has a few recipes and a few batches for those recipes'
+
+    def handle(self, *args, **options):
+        demoUser = User.objects.create_user(username='demoUser', password='demoUser123', email='demoUser@yeastmode.com')
+        demoRecipe1 = Recipe.objects.create(name='Saison', brew_type='Beer', style='Saison', description='This is a mild saison.', owner=demoUser) 
+        demoSession1 = BrewSession.objects.create(recipe=demoRecipe1, batch_number='1', status='fermenting', brew_date='2026-07-14', original_gravity='1.058')
+        demoRecipe2 = Recipe.objects.create(name='Cyber Red', brew_type='Beer', style='Red Ale', description='This is an Irish Red Ale.', owner=demoUser)
+        demoSession2 = BrewSession.objects.create(recipe=demoRecipe2, batch_number='1', status='fermenting', brew_date='2026-07-14', original_gravity='1.062')
+        self.stdout.write(self.style.SUCCESS('Seed data created successfully!')) 
+        
